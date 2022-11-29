b19fa5bfac1472247397c16088c50a2251ea1a81
Petru Orbu <whhroo@Petrus-MacBook-Pro.local>
2022-11-29 17:50:48 +0200

    v1.0

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Product({ product, get }) {
     return (
         <article onClick={() => get && get(product._id)} className="catalog-body__item product">
-            <img src={product && product.images[0]} alt="" />
+            <img src={product && product.images.length > 0 && product.images[0]} alt="" />
             <p>{product.name}</p>
             <h4>{product.price} MDL</h4>
         </article>
