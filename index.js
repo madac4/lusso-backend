ed59f78125bf9ff4ef2ee54ca4b6a815b5de9b1b
Petru Orbu <whhroo@Petrus-MacBook-Pro.local>
2022-11-29 17:46:40 +0200

    v1.0

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const path = require('path');
 var cors = require('cors');
 
 const productRoute = require('./routes/product');
